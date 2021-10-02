63523ce4f4a37c868123e87d38662141e658fdfe
Beatriz Tavares <beatriz98t@gmail.com>
2021-10-02 17:24:17 -0300

    feat: completa desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 
 import '../styles/tasklist.scss'
-
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
 
 interface Task {
@@ -16,14 +15,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
-  }
+    if(!newTaskTitle) return;
+    const newTask = {
+      id: tasks.length =+ 1 ,
+      title: newTaskTitle,
+      isComplete: false,
+    };
+
+  setTasks(oldState => [...oldState, newTask]);
+  setNewTaskTitle('');
+}
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+ 
+    let newTasks:Task[] ;
+    newTasks=[];
+    newTasks = tasks.map((task)=>{
+      if(task.id === id)  !task.isComplete
+      return task
+    })
+    setTasks([...newTasks]);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter((task)=> task.id !== id)
+    setTasks([...newTasks])
   }
 
   return (
